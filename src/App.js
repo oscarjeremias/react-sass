5756313fc525b579ba0ff8ed2994c5f2ae33138e
oscarjeremias <oscarjeremias3@gmail.com>
2022-07-25 03:57:12 +0100

    app finalizado

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="container">
+      <header className="container--header">
+        <div className="header-menu">
+        menu
+        </div>
+        <div className="header-fotoPefil">
+        perfil
+        </div>
+      </header>
+      <main className="container-main"></main>
+      <footer className="container-footer"></footer>
+    </div>
+  )
+}
+
+export default App
